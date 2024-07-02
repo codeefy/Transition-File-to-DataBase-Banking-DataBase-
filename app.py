7f6af20af4f5524f8aba1348e0dde4f260e6bc43
[200~https://github.com/codeefy~ <cashleyrohit01@gmail.com>
2024-07-02 17:51:53 +0530

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import time # Importing the time module to calculate the time taken for the tran
 # Schema of Student table
 col_name_Account = ['Account_no', 'Name', 'Address', 'Phone_no', 'PAN', 'A/C type', 'Balance'] # Schema of Account table in csv
 col_name_Ledger = ['Account1', 'Account2', 'Amount', 'D/C'] # Schema of Ledger table in csv format  
-# ledger table used for 
+# ledger 
 if path.exists('.\\Accounts.csv') == False and path.exists('.\\Ledger.csv') == False: # Checking if the files are already present or not 
     f_ob = open('Accounts.csv', 'a+') # Creating a new file for Account table if not present  
     f_wr = csv.DictWriter(f_ob, fieldnames=col_name_Account)  # Writing the schema to the file
@@ -19,7 +19,6 @@ if path.exists('.\\Accounts.csv') == False and path.exists('.\\Ledger.csv') == F
     f_wr = csv.DictWriter(f_ob, fieldnames=col_name_Ledger) # Writing the schema to the file 
     f_wr.writeheader() # Writing the header to the file
     f_ob.close() # Closing the file  
-
 
 # utility methods for the operations on the tables 
 def add_Account(account_no, name, address, phone_no, pan, type, balance): # Method to add a record to the Account table  
